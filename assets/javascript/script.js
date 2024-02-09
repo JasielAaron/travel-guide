c88454b6a20e96395381503618d1fa1c94b67dc3
EzraJRogers <Rogersezra7@gmail.com>
2024-02-08 19:00:34 -0700

    Fixed modal

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,54 +33,32 @@ searchButton.addEventListener('click', () => {
 
 })
 
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("myModal");
+    var overlay = document.getElementById("overlay");
+    modal.style.display = "block";
+    overlay.style.display = "block";
+});
 
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    var overlay = document.getElementById("overlay");
+    
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
 
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById("myModal");
+    var overlay = document.getElementById("overlay");
+    modal.style.display = "block";
+    overlay.style.display = "block";
+});
 
-
-
-
-
-
-
-//document.addEventListener('DOMContentLoaded', function() {
-//const searchButton = document.getElementById('searchButton');
-//searchButton.addEventListener('click', function() {
-//const destination = document.getElementById('destinationInput').value;
-//fetchCountryInfo(destination);
-//});
-//});
-
-//function fetchCountryInfo(countryName) {
-//const apiKey = '5effe679202bb71692f127fed032bc1b';
-//const apiUrl = `https://api.countrylayer.com/v2/name/${countryName}?access_key=${apiKey}`;
-
-//fetch(apiUrl)
-//.then(response => {
-//if (!response.ok) {
-//throw new Error('Network response was not ok');
-//}
-//return response.json();
-//})
-//.then(data => {
-//if (data && data.length > 0) {
-//const country = data[0]; // Assuming the first result is the most relevant
-//displayCountryInfo(country);
-//} else {
-//console.log('No data found for the specified country');
-//}
-//})
-//.catch(error => {
-//console.error('There has been a problem with your fetch operation:', error);
-//});
-//}
-
-
-//function displayCountryInfo(country) {
-//document.getElementById('countryNameResult').textContent = country.name;
-//document.getElementById('capitalResult').textContent = country.capital;
-//document.getElementById('capitalRes').textContent = country.capital;
-// Add more elements as needed, ensuring HTML has corresponding IDs
-
-
-//}
-
+function closeModal() {
+    var modal = document.getElementById("myModal");
+    var overlay = document.getElementById("overlay");
+    
+    modal.style.display = "none";
+    overlay.style.display = "none";
+}
